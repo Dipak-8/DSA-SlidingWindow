1e5c5821acef4d4c3664a23a8b4a9aaf38b73c1c
Dipak Das <dipakdas84630@gmail.com>
2025-03-02 11:05:56 +0530

    Time: 8 ms (84.88%), Space: 44.8 MB (60.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l = 0, r = 0;
+        int l = 0, r = 0;  
         int n = s.length();
-        int maxLen = 0;
-        int maxFreq = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;  
+        int maxFreq = 0;  
+        int[] hash = new int[26]; 
 
-        while(r < n) {
-            char c =  s.charAt(r);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            maxFreq = Math.max(maxFreq, map.get(c));
+        while (r < n) {
+            char c = s.charAt(r);
+            hash[c - 'A']++; 
+            // Track the highest occurring character in the window
+            maxFreq = Math.max(maxFreq, hash[c - 'A']); 
             int len = r - l + 1;
-            int ct = len - maxFreq;
-            
-            if(ct > k) {
-                char ch = s.charAt(l);
-                map.put(ch, map.getOrDefault(ch, 0) - 1);
+            // Count of characters to be replaced
+            int ct = len - maxFreq; 
+             // If replacements exceed k, shrink window from the left
+            if (ct > k) {
+                hash[s.charAt(l) - 'A']--;
                 l++;
-            } else {
+            } else { // Update the maximum valid window size
                 maxLen = Math.max(maxLen, len);
             }
             r++;
